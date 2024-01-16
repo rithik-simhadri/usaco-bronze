7d182091cf0a1c4cb88b96f7ed6cc1701a8dd905
rithik-simhadri <149263516+rithik-simhadri@users.noreply.github.com>
2024-01-15 18:04:36 -0700

    Add files via upload

---
+++ where_am_i_correct.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+using namespace std;
+
+int main() {
+    freopen("whereami.in", "r", stdin);
+	freopen("whereami.out", "w", stdout);
+
+    int n;
+    vector<string> stringSplitted;
+    string colors;
+    set<string> noDups;
+
+    cin >> n >> colors;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j <= n - i; j++){
+            stringSplitted.push_back(colors.substr(j, i));
+            noDups.insert(colors.substr(j, i));
+            // cout << i << " " << j << endl;
+        }
+
+        if (noDups.size() == stringSplitted.size()){
+            cout << i << endl;
+            break;    
+        } else {
+            noDups.erase(noDups.begin(), noDups.end());
+            stringSplitted.erase(stringSplitted.begin(), stringSplitted.end());
+        }
+        
+    }
+}
