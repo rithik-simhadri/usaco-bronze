94c7025ecc876f1cf1b3483d549667ce17ed5389
rithik-simhadri <149263516+rithik-simhadri@users.noreply.github.com>
2024-03-25 21:46:22 -0700

    mad_scientist.cpp

--- mad_scientist.cpp
+++ mad_scientist.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
     freopen("breedflip.in", "r", stdin);
-	freopen("breedflip.out", "w", stdout);
+    freopen("breedflip.out", "w", stdout);
     
     int n;
     cin >> n;
