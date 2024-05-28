a06c89470475198cd72ceeff9b6fc053b2397f49
rithik-simhadri <149263516+rithik-simhadri@users.noreply.github.com>
2024-05-28 13:26:54 -0700

    Add files via upload

---
+++ diamond_collectors.cpp
@@ -0,0 +1,35 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ifstream fin ("diamond.in");
+    ofstream fout ("diamond.out");
+
+    int N; fin >> N;
+    int K; fin >> K;
+
+    vector<int> sizes;
+    for (int i = 0; i < N; i++) {
+        int diamond; fin >> diamond;
+        sizes.push_back(diamond);
+    }
+    
+    sort(sizes.begin(), sizes.end());
+    int counter = 0;
+    int maxcount = 0;
+    for (int i = 0; i < N; i++) {
+        maxcount = max(counter, maxcount);
+        counter = 0;
+        for (int j = i+1; j < N; j++){
+            if (abs(sizes[i] - sizes[j]) <= K) {
+                counter++;
+            }
+        }
+    }
+
+    maxcount += 1;
+    fout << maxcount << endl;
+}
