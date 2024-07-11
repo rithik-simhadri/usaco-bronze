bc353d7d4c52c8dd5960fbab73cb765b51023169
Rithik Simhadri <149263516+rithik-simhadri@users.noreply.github.com>
2024-07-10 17:49:44 -0700

    bovine_genomics_new.cpp

--- bovine_genomics_new.cpp
+++ bovine_genomics_new.cpp
@@ -7,23 +7,23 @@ using namespace std;
 
 int main() {
 	ifstream fin ("cownomics.in");
-    ofstream fout ("cownomics.out");
-
+    	ofstream fout ("cownomics.out");
+	
 	int n, m;
 	fin >> n >> m;
 
-    vector<vector<char>> plain(n, vector<char>(m));
+    	vector<vector<char>> plain(n, vector<char>(m));
 	vector<vector<char>> spots(n, vector<char>(m));
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-            fin >> spots[i][j];
-        }
+            		fin >> spots[i][j];
+        	}
 	}
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-            fin >> plain[i][j];
-        }
+            		fin >> plain[i][j];
+        	}
 	}
 
 	int ANS = 0;
