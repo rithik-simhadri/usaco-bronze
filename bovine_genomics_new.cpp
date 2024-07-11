14368db2530b77bb7944841f5032b1fff944db5e
Rithik Simhadri <149263516+rithik-simhadri@users.noreply.github.com>
2024-07-10 17:35:18 -0700

    Add files via upload

---
+++ bovine_genomics_new.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ifstream fin ("cownomics.in");
+    ofstream fout ("cownomics.out");
+
+	int n, m;
+	fin >> n >> m;
+
+    vector<vector<char>> plain(n, vector<char>(m));
+	vector<vector<char>> spots(n, vector<char>(m));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+            fin >> spots[i][j];
+        }
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+            fin >> plain[i][j];
+        }
+	}
+
+	int ANS = 0;
+	for (int i = 0; i < m; i++) {
+		bool duplicate = false;
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < n; k++) {
+				if (spots[j][i] == plain[k][i]) {
+					duplicate = true;
+					break;
+				}
+			}
+		}
+
+		if (!duplicate) ANS++;
+	}
+
+	fout << ANS << endl;
+}
